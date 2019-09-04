d45d2093688af9368b7a11dd26e91ae5058f3f11
Bekbolot <bekbolot@gmail.com>
2019-09-04 12:59:01 +0600

    Add model NotificationsThread Write validation fot NotificationThread Write test for validation

--- model/model_test.go
+++ model/model_test.go
@@ -3,6 +3,7 @@ package model
 import (
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -121,5 +122,21 @@ func TestStanduper(t *testing.T) {
 		if err != nil {
 			assert.Equal(t, errors.New(tt.errorMessage), err)
 		}
+	}
+}
+
+func TestNotificationThread(t *testing.T) {
+	var tests = []struct {
+		n   NotificationThread
+		exp error
+	}{
+		{NotificationThread{ChatID: int64(0), Username: "User1", NotificationTime: time.Now(), ReminderCounter: 0}, errors.New("Field ChatID is empty")},
+		{NotificationThread{ChatID: int64(1), Username: "", NotificationTime: time.Now(), ReminderCounter: 0}, errors.New("Field Username is empty")},
+		{NotificationThread{ChatID: int64(1), Username: "User1", NotificationTime: time.Now(), ReminderCounter: -1}, errors.New("Field ReminderCounter is empty")},
+		{NotificationThread{ChatID: int64(1), Username: "User1", NotificationTime: time.Now(), ReminderCounter: 1}, nil},
+	}
+	for _, e := range tests {
+		res := Validate(e.n)
+		assert.Equal(t, res, e.exp)
 	}
 }
